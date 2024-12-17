4338fb159e5b9df1e621ce93859109db13624458
vahid in F-org <you@example.com>
2024-12-17 12:13:25 +0330

    first commit

---
+++ picplay/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace picplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBoxImages = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ListBoxImages
+            // 
+            this.ListBoxImages.AllowDrop = true;
+            this.ListBoxImages.FormattingEnabled = true;
+            this.ListBoxImages.ItemHeight = 16;
+            this.ListBoxImages.Location = new System.Drawing.Point(12, 12);
+            this.ListBoxImages.Name = "ListBoxImages";
+            this.ListBoxImages.Size = new System.Drawing.Size(67, 100);
+            this.ListBoxImages.TabIndex = 0;
+            this.ListBoxImages.SelectedIndexChanged += new System.EventHandler(this.ListBoxImages_SelectedIndexChanged);
+            this.ListBoxImages.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListBoxImages_DragDrop);
+            this.ListBoxImages.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListBoxImages_DragEnter);
+            this.ListBoxImages.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ListBoxImages_KeyDown);
+            this.ListBoxImages.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListBoxImages_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ListBoxImages);
+            this.Name = "Form1";
+            this.Text = "PicPlay";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBoxImages;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ picplay/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace picplay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBoxImages_DragEnter(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void ListBoxImages_DragDrop(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void ListBoxImages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            changeimage();
+        }
+        private void changeimage()
+        {
+            if (ListBoxImages.SelectedItem != null) // چک کردن انتخاب‌بودن آیتم
+            {
+                string selectedImagePath = ListBoxImages.SelectedItem.ToString();
+                try
+                {
+                    pictureBox1.Image = System.Drawing.Image.FromFile(selectedImagePath);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // تنظیم حالت نمایش عکس
+                    this.Text = "PicPlay | vahidzahani.ir | " + ListBoxImages.Items.Count.ToString() + " items";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("خطا در نمایش عکس: " + ex.Message);
+                }
+            }
+
+        }
+        private void SaveListBoxToFile()
+        {
+            
+
+            try
+            {
+                // مسیر فایل
+                string filePath = "filelist.txt";
+
+                // باز کردن فایل برای نوشتن
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    foreach (var item in ListBoxImages.Items)
+                    {
+                        sw.WriteLine(item.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("خطا در ذخیره لیست: " + ex.Message);
+            }
+        }
+        private void LoadListBoxFromFile()
+        {
+            try
+            {
+                string filePath = "filelist.txt";
+
+                // بررسی وجود فایل
+                if (File.Exists(filePath))
+                {
+                    using (StreamReader sr = new StreamReader(filePath))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            ListBoxImages.Items.Add(line);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("خطا در بارگذاری لیست: " + ex.Message);
+            }
+        }
+
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy; // نشان دادن آیکن کپی
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            // دریافت مسیر فایل‌ها
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            // فیلتر کردن فایل‌های عکس بر اساس پسوند
+            var imageFiles = files.Where(file =>
+                Path.GetExtension(file).Equals(".jpg", StringComparison.OrdinalIgnoreCase) ||
+                Path.GetExtension(file).Equals(".jpeg", StringComparison.OrdinalIgnoreCase) ||
+                Path.GetExtension(file).Equals(".png", StringComparison.OrdinalIgnoreCase) ||
+                Path.GetExtension(file).Equals(".bmp", StringComparison.OrdinalIgnoreCase) ||
+                Path.GetExtension(file).Equals(".gif", StringComparison.OrdinalIgnoreCase));
+
+            // اضافه کردن فایل‌های عکس به ListBox
+            foreach (var file in imageFiles)
+            {
+                if (!ListBoxImages.Items.Contains(file)) // جلوگیری از اضافه شدن تکراری
+                {
+                    ListBoxImages.Items.Add(file);
+                }
+            }
+            ListBoxImages.SelectedIndex = 0;
+            SaveListBoxToFile();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadListBoxFromFile();
+            if (ListBoxImages.Items.Count > 0) { ListBoxImages.SelectedIndex = 0; }
+        }
+
+        private void ListBoxImages_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void ListBoxImages_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                // بررسی اینکه آیا آیتمی انتخاب شده است
+                if (ListBoxImages.SelectedItem != null)
+                {
+                    // حذف آیتم انتخاب‌شده
+                    ListBoxImages.Items.Remove(ListBoxImages.SelectedItem);
+
+                    // ذخیره وضعیت جدید لیست‌باکس
+                    SaveListBoxToFile();
+                    if (ListBoxImages.Items.Count > 0)
+                    {
+                        ListBoxImages.SelectedIndex = 0;
+                        changeimage();
+                    }
+                }
+            }
+            if (e.KeyCode==Keys.Down)
+            {
+                if (ListBoxImages.SelectedIndex == ListBoxImages.Items.Count -1)
+                {
+                    ListBoxImages.SelectedIndex = 0;
+                    e.Handled = true;
+                    
+                }
+            }
+        }
+    }
+}
